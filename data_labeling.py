8592ff175b0c59f8c7739caf84cf0bb5a7714dc7
riku <ymmtr666@gmail.com>
2019-01-21 20:26:40 +0900

    fix datalabeling

--- data_labeling.py
+++ data_labeling.py
@@ -2,31 +2,43 @@
 
 import os
 import random
+import argparse
 
 ROOT = "./data"
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num", default=-1, type=int)
+    parser.add_argument("--shuffle", "-s", action="store_false")
+    args = parser.parse_args()
+
     array = []
-    count = 1000
-    
+    count = args.num
+
+    gist_path = os.path.join(ROOT, "gist")
+    other_path = os.path.join(ROOT, "other")
+
+    if args.shuffle:
+        random.shuffle(gist_path)
+        random.shuffle(other_path)
+
+    gist_list = [filename for filename in listdir(gist_path) if not filename.startswith('.')]
+    other_list = [filename for filename in listdir(other_path) if not filename.startswith('.')]
+
+
     i = 0
-
-    gist_path = os.listdir(os.path.join(ROOT, "gist"))
-    random.shuffle(gist_path)
-
-    other_path = os.listdir(os.path.join(ROOT, "gist"))
-    random.shuffle(other_path)
-    
-    for img_path in gist_path:
+    for img_path in gist_list:
         array.append( [os.path.join(ROOT, "gist", img_path), 0])
-        if i == count - 1:
+        if i == count - 1 and count != -1:
             break
         else:
             i+=1
 
     i = 0
-    for img_path in other_path:
+    for img_path in other_list:
         array.append( [os.path.join(ROOT, "other", img_path), 1])
-        if i == count - 1:
+        if i == count - 1 and count != -1:
             break
         else:
             i+=1
