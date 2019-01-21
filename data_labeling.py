37e8a8616cfc4e71d7011f8f510e193528057751
riku <ymmtr666@gmail.com>
2019-01-21 19:30:57 +0900

    fix data_labeling.py

--- data_labeling.py
+++ data_labeling.py
@@ -5,12 +5,23 @@ import os
 ROOT = "./data"
 if __name__ == "__main__":
     array = []
+    count = 30
     
+    i = 0
     for img_path in os.listdir(os.path.join(ROOT, "gist")):
         array.append( [os.path.join(ROOT, "gist", img_path), 0])
+        if i == count:
+            break
+        else:
+            i+=1
 
+    i = 0
     for img_path in os.listdir(os.path.join(ROOT, "other")):
         array.append( [os.path.join(ROOT, "other", img_path), 1])
+        if i == count:
+            break
+        else:
+            i+=1
     
     with open("label.txt", "w") as f:
         for path, number in array:
