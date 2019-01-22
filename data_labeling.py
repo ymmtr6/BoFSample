dc02bd658a3eeb561e1c0940da76471f5b7fb79b
riku <ymmtr666@gmail.com>
2019-01-22 12:11:38 +0900

    fucntioning

--- data_labeling.py
+++ data_labeling.py
@@ -11,30 +11,11 @@ def write(filepath, array):
         for path, number in array:
             f.write("{} {}\n".format(path, number))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--output", "-o", default="label", type=str)
-    args = parser.parse_args()
-
+def dist(gist_list, other_list, target):
+    """
+    """
     train = []
     test = []
-
-    gist_path = os.path.join(ROOT, "gist")
-    other_path = os.path.join(ROOT, "other")
-
-    gist_list = [filename for filename in os.listdir(gist_path) if not filename.startswith('.')]
-    other_list = [filename for filename in os.listdir(other_path) if not filename.startswith('.')]
-
-    targets = []
-
-    for i in range(32):
-        targets.append("g{}-".format(i+1))
-    
-    for i in range(11):
-        targets.append("o{}-".format(i+1))
-    
-    target = targets[0]
-
     for img_path in gist_list:
         p = os.path.join(ROOT, "gist", img_path)
         if img_path.startswith(target):
@@ -49,10 +30,34 @@ if __name__ == "__main__":
         else:
             train.append([p, 0])
     
-    with open("{}-{}-train.txt".format(args.output, target), "w") as f:
+    with open("{}-{}train.txt".format(args.output, target), "w") as f:
         for path, number in train:
             f.write("{} {}\n".format(path, number))
 
-    with open("{}-{}-test.txt".format(args.output, target), "w") as f:
+    with open("{}-{}test.txt".format(args.output, target), "w") as f:
         for path, number in test:
             f.write("{} {}\n".format(path, number))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", "-o", default="label", type=str)
+    args = parser.parse_args()
+
+    gist_path = os.path.join(ROOT, "gist")
+    other_path = os.path.join(ROOT, "other")
+
+    gist_list = [filename for filename in os.listdir(gist_path) if not filename.startswith('.')]
+    other_list = [filename for filename in os.listdir(other_path) if not filename.startswith('.')]
+
+    targets = []
+    for i in range(32):
+        targets.append("g{}-".format(i+1))
+    for i in range(11):
+        targets.append("o{}-".format(i+1))
+    
+    for target in targets:
+        dist(gist_list, other_list, target)
+
+    
+    
